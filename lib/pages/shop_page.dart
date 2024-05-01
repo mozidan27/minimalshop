d8650ece2b950506700658d96eabce3d55de3e03
mozidan27 <153581941+mozidan27@users.noreply.github.com>
2024-05-01 09:02:56 +0300

    complete product tile and make add button active

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,21 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:minimalshop/models/product.dart';
+import 'package:minimalshop/models/shop.dart';
 import 'package:minimalshop/widgets/my_drawer.dart';
+import 'package:minimalshop/widgets/my_product_tile.dart';
+import 'package:provider/provider.dart';
 
 class ShopPage extends StatelessWidget {
   const ShopPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final products = context.watch<Shop>().shop;
+
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        title: Text(
-          "Shop Page",
-          style: TextStyle(color: Theme.of(context).colorScheme.inversePrimary),
+        backgroundColor: Theme.of(context).colorScheme.background,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          title: Text(
+            "Shop Page",
+            style:
+                TextStyle(color: Theme.of(context).colorScheme.inversePrimary),
+          ),
         ),
-      ),
-      drawer: const MyDrawer(),
-    );
+        drawer: const MyDrawer(),
+        body: ListView(
+          children: [
+            const SizedBox(
+              height: 25,
+            ),
+            // shop subtitle
+            Center(
+              child: Text(
+                'Pick from a selected list of our premium products',
+                style: TextStyle(
+                    color: Theme.of(context).colorScheme.inversePrimary),
+              ),
+            ),
+
+            // product list
+            SizedBox(
+              height: 550,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: products.length,
+                padding: const EdgeInsets.all(15),
+                itemBuilder: (context, index) {
+                  //get each indivaidual product from  shop
+                  final Product product = products[index];
+                  return MyProductTile(product: product);
+                  // return as product tile
+                },
+              ),
+            ),
+          ],
+        ));
   }
 }
---
+++ lib/widgets/my_product_tile.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:minimalshop/models/product.dart';
+import 'package:minimalshop/models/shop.dart';
+import 'package:provider/provider.dart';
+
+class MyProductTile extends StatelessWidget {
+  final Product product;
+
+  const MyProductTile({super.key, required this.product});
+// add to cart method
+  void addToCart(BuildContext context) {
+    // show a dialog box to confirm to add the item or not
+
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: const Text("Add this item to your cart?"),
+        actions: [
+          // cancel button
+
+          MaterialButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text('Cancel'),
+          ),
+
+          // yes button
+          MaterialButton(
+            onPressed: () {
+              Navigator.pop(context);
+              // add the item to the cart
+              context.read<Shop>().addToCart(product);
+            },
+            child: const Text('Yes'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.primary,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(25),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // product image
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              AspectRatio(
+                aspectRatio: 1,
+                child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.secondary,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  padding: const EdgeInsets.all(25),
+                  child: const Icon(Icons.favorite),
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+
+              //product name
+              Text(
+                product.name,
+                style:
+                    const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              // product description
+              Text(
+                product.description,
+                style: TextStyle(
+                    color: Theme.of(context).colorScheme.inversePrimary),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+          //product price
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "EGP ${product.price.toString()}",
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.secondary,
+                    borderRadius: BorderRadius.circular(8)),
+                child: IconButton(
+                  onPressed: () => addToCart(context),
+                  icon: const Icon(Icons.add),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
